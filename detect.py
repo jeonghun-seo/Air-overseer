d498375a19619a8b0400471333c177e3666182d0
JeongHun-Seo <jeonghun.seo@outlook.kr>
2023-10-22 20:27:58 +0900

    add new features

--- detect.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO 
-
-def run(): #실시간 객체감지 테스트
-    model = YOLO('models/yolov8n.pt')
-    result = model.predict(source=0,show=True)
-    return 0
-
-if __name__=="__main__":
-    print(__name__)
